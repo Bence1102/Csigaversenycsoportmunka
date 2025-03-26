c42aecaf6ee43d3b56b0aa2aa718e3a80769e3b7
Bence1102 <szente.bence04@gmail.com>
2025-03-26 09:23:08 +0100

    csigas

--- src/main/Csiga.java
+++ src/main/Csiga.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package main;
 
-/**
- *
- * @author SzenteBence(Szf_N_20
- */
+
 public class Csiga {
-    
+     private String szin = "Piros,Kék,Zöld";
+     private static String abra = "🐌";
+     private boolean gyorsitas;
 }
