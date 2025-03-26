5e4894600f0c4a01d5d5dc1cd3e7e55833621541
Bence1102 <szente.bence04@gmail.com>
2025-03-26 09:52:46 +0100

    settergetter

--- src/main/Csiga.java
+++ src/main/Csiga.java
@@ -1,30 +1,56 @@
-
 package main;
 
-
 public class Csiga {
-     private String szin;
-     private String abra;
-     private boolean gyorsitas;
-     private String szinKod;
-     
-     
-     
-     
-     public Csiga(String szin,String szinKod,String abra){
-         this.szin=szin;
-         this.szinKod=szinKod;
-         this.gyorsitas=false;
-         this.abra=abra;
-     }
-     
-     
-     
-     
-     public int megy(){
-         if(Math.random()<0.2){
-             gyorsitas=true;
-         }
-         return gyorsitas ? 2:1;
-     }
+
+    private String szin;
+    private String abra;
+    private boolean gyorsitas;
+    private String szinKod;
+
+    public Csiga(String szin, String szinKod, String abra) {
+        this.szin = szin;
+        this.szinKod = szinKod;
+        this.gyorsitas = false;
+        this.abra = abra;
+    }
+
+    public int megy() {
+        if (Math.random() < 0.2) {
+            gyorsitas = true;
+        }
+        return gyorsitas ? 2 : 1;
+    }
+
+    public String getSzin() {
+        return szin;
+    }
+
+    public void setSzin(String szin) {
+        this.szin = szin;
+    }
+
+    public String getSzinKod() {
+        return szinKod;
+    }
+
+    public void setSzinKod(String szinKod) {
+        this.szinKod = szinKod;
+    }
+
+    public boolean getGyorsitas() {
+        return gyorsitas;
+    }
+
+    public void setGyorsitas(boolean gyorsitas) {
+        this.gyorsitas = gyorsitas;
+    }
+
+    public String getAbra() {
+        return abra;
+    }
+
+    public void setAbra(String abra) {
+        this.abra = abra;
+    }
+
 }
