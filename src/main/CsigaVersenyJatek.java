1e58be186db3d6254e9b536962973891180ad9c5
MGergo20 <medgyaszayg3@gmail.com>
2025-03-26 10:30:10 +0100

    10:30

--- src/main/CsigaVersenyJatek.java
+++ src/main/CsigaVersenyJatek.java
@@ -3,8 +3,17 @@ package main;
 
 
 public class CsigaVersenyJatek {
-    private Csiga[] Csigak;
-   
+    
+    private Csiga[] csigak;
+    private int[] megy;
+    private int kor;
+    
+    public CsigaVersenyJatek(Csiga[] csigak, int kor){
+        this.csigak=csigak;
+        this.kor=kor;
+        this.megy=new int[csigak.length];
+    }
+    
     public static void main(String[] args) {
         
     }
