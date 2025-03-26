15a1002ef4a5751c693036a6631863039e5b678e
MGergo20 <medgyaszayg3@gmail.com>
2025-03-26 10:38:24 +0100

    CsigaVersenySetterGetter

--- src/main/CsigaVersenyJatek.java
+++ src/main/CsigaVersenyJatek.java
@@ -13,6 +13,31 @@ public class CsigaVersenyJatek {
         this.kor=kor;
         this.megy=new int[csigak.length];
     }
+     public Csiga[] getCsigak() {
+        return csigak;
+    }
+    
+    public void setCsigak(Csiga[] csigak) {
+        this.csigak = csigak;
+    }
+    
+    
+    public int[] getMegy() {
+        return megy;
+    }
+    
+    public void setMegy(int[] megy) {
+        this.megy = megy;
+    }
+    
+   
+    public int getKor() {
+        return kor;
+    }
+    
+    public void setKor(int kor) {
+        this.kor = kor;
+    }
     
     public static void main(String[] args) {
         
